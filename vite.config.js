0f4e96fed242cb0ea417eb9a0f329b971fe49647
MuhJumarawinoto <muhamadjumarawinoto.77@gmail.com>
2023-06-26 12:28:51 +0700

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'dist', // Direktori output hasil build
-    emptyOutDir: false, // Jangan hapus konten direktori output sebelum build
-    rollupOptions: {
-      input: {
-        main: './index.html' // File HTML utama yang akan menjadi entry point
-      }
-    }
-  }
 })
