7b882764545d0ee5a968e09ab7840214be4a37f3
MuhJumarawinoto <muhamadjumarawinoto.77@gmail.com>
2023-06-26 11:25:46 +0700

    tersisa hanya tampilan saja

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist', // Direktori output hasil build
+    emptyOutDir: false, // Jangan hapus konten direktori output sebelum build
+    rollupOptions: {
+      input: {
+        main: './index.html' // File HTML utama yang akan menjadi entry point
+      }
+    }
+  }
 })
